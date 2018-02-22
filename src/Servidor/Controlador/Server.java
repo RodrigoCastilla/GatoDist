7768da0c818c16e237eaa6170b42fe5f39d8f8bb
Rodrigo <armc3po@gmail.com>
2018-02-22 12:32:12 -0600

    SERVER

---
+++ src/Servidor/Controlador/Server.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servidor.Controlador;
+
+import Servidor.Modelo.Jugador;
+import Servidor.Modelo.ModeradorJuego;
+import Servidor.Modelo.Movimiento;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ARMC3PO
+ */
+public class Server {
+    public static void main(String[] args) throws IOException {
+        String clientSentence;
+        String respuesta;
+        int cantidadJugadores = 0;
+        int valorAlAzarEntreUnoYCien = (int) (Math.random()*101 + 1);
+        boolean seleccionDeTurnos = false;
+        boolean seleccionSimbolo = false;
+        boolean manejoDeJuego = false;
+        ArrayList<Movimiento> movimientos = new ArrayList<Movimiento>();
+        ModeradorJuego moderador= new ModeradorJuego(movimientos);
+        ArrayList<Jugador> jugadores = new ArrayList<Jugador> ();
+        ServerSocket listener = new ServerSocket(9091);
+        try {
+            while (true) {
+                Socket socket = listener.accept();
+                try {
+                    respuesta = "";
+                    BufferedReader inFromClient =
+                    new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                     DataOutputStream outToClient = new DataOutputStream(socket.getOutputStream());
+                     clientSentence = inFromClient.readLine();
+                     System.out.println("Received: " + clientSentence);
+                     
+                     
+                     
+                     //Registro de jugadores
+                     if((clientSentence.toUpperCase()).compareTo("SOLICITUD") == 0){
+                         String respuestaSolicitud="";
+                         if(cantidadJugadores == 0){
+                             jugadores.add(new Jugador("JUGADOR1"));
+                             respuestaSolicitud = "JUGADOR1";
+                              System.out.println("Registrado JUGADOR1");
+                         cantidadJugadores++;
+                         } else if (cantidadJugadores ==1){
+                             jugadores.add(new Jugador("JUGADOR2"));
+                             respuestaSolicitud = "JUGADOR2";
+                             System.out.println("Registrado JUGADOR 2");
+                             cantidadJugadores++;
+                         } else if(cantidadJugadores >1){
+                             respuestaSolicitud="SALA_LLENA";
+                         }
+                         respuesta = respuestaSolicitud;
+                         
+                     }
+                     
+                     
+                     
+                     if(((clientSentence.toUpperCase()).compareTo("CLOSECXN")) == 0){
+                         outToClient.writeBytes("Connection Closed");
+                         socket.close();
+                         listener.close();
+                         break;
+                     }
+                     outToClient.writeBytes(respuesta);
+                     
+                } finally {
+                    socket.close();
+                }
+            }
+        }
+        finally {
+            listener.close();
+        }
+    }
+}
--- src/Servidor/Controlador/testFrame.java
+++ src/Servidor/Controlador/testFrame.java
@@ -11,6 +11,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -38,6 +39,8 @@ public class testFrame extends javax.swing.JFrame {
 
         jLabel1 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -50,6 +53,10 @@ public class testFrame extends javax.swing.JFrame {
             }
         });
 
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -57,9 +64,10 @@ public class testFrame extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(149, 149, 149)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 338, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 155, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(174, Short.MAX_VALUE))
+                .addContainerGap(76, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -68,7 +76,9 @@ public class testFrame extends javax.swing.JFrame {
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(28, 28, 28)
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(277, Short.MAX_VALUE))
+                .addGap(36, 36, 36)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(61, Short.MAX_VALUE))
         );
 
         pack();
@@ -77,7 +87,12 @@ public class testFrame extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         try {
             ProxyCliente proxyC = new ProxyCliente();
-            jLabel1.setText(proxyC.mandarMensajeAlServidor("CLOSECXN"));
+            jLabel1.setText(proxyC.mandarMensajeAlServidor("SOLICITUD"));
+            //ArrayList<String> movs = proxyC.obtenerDatosTablero();
+            //String text = "";
+            /*for(int i=0; i<movs.size(); i++)
+                text+=movs.get(i);
+            jTextArea1.setText(text);*/
         } catch (IOException ex) {
             Logger.getLogger(testFrame.class.getName()).log(Level.SEVERE, null, ex);
         } catch (InterruptedException ex) {
@@ -123,5 +138,7 @@ public class testFrame extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextArea1;
     // End of variables declaration//GEN-END:variables
 }
